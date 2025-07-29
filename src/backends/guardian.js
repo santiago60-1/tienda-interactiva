d74fd6e9314f22a3c522a188d9a9e29a69698ca1
santiago ortega <santiago59782@gmail.com>
2025-07-28 21:58:11 -0500

    arreglado el cerrar sesion

--- src/backends/guardian.js
+++ src/backends/guardian.js
@@ -2,7 +2,7 @@ function checkSession (){
     const user = localStorage.getItem('user');
     
     if (user === null) {
-        window.location.href = "../../index.html";
+        window.location.href = "/";
     }
 }
 
--- src/backends/logout.js
+++ src/backends/logout.js
@@ -1,9 +1,14 @@
 function cerrarSesion() {
-    // Clear user data from localStorage
-    localStorage.removeItem('user');
+    // Verificar si 'user' existe en localStorage
+    if (localStorage.getItem('user')) {
+        console.log("Cerrando sesión...");
+        localStorage.removeItem('user');
+    } else {
+        console.log("No hay usuario en sesión.");
+    }
     
-    // Redirect to the login page
+    // Redirigir a la página de login
     window.location.href = "../../index.html";
 }
 
-document.getElementById('logoutButton').addEventListener('click', cerrarSesion);
+document.getElementById('logout-btn').addEventListener('click', cerrarSesion);
